8dbfa679375bb64a3334e2b27af9067e8d2758b8
ChixenCounter <77216787+ChixenCounter@users.noreply.github.com>
2022-11-04 13:16:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+function random_item(items)
+{
+  
+return items[Math.floor(Math.random()*items.length)];
+     
+}
+
+var items = [254, 45, 212, 365, 2543];
+
+
 const textarea = document.getElementById('message');
 
 // ✅ Append text
@@ -7,5 +17,5 @@ const btn = document.getElementById('btn');
 
 // ✅ Append text on button click
 btn.addEventListener('click', function handleClick() {
-  textarea.value += 'Appended text';
+  textarea.value += items;
 });
