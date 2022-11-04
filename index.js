1f7fa1d2670dbd49774bcddd061cf3086454ee35
ChixenCounter <77216787+ChixenCounter@users.noreply.github.com>
2022-11-04 13:10:43 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const textarea = document.getElementById('message');
+
+// ✅ Append text
+textarea.value += 'Appended text';
+
+const btn = document.getElementById('btn');
+
+// ✅ Append text on button click
+btn.addEventListener('click', function handleClick() {
+  textarea.value += 'Appended text';
+});
